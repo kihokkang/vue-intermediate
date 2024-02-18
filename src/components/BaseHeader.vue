7e0f0bbfbe515c4f14b179d94d3bc863b73a1b13
Giho <kihokkang2@gmail.com>
2024-02-18 16:08:26 +0900

    기본 컴포넌트 설계

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- 템플릿 태그 바로 아래에는 하나의 태그만 존재 해야한다(Root)-->
+    <header>
+        <h1>TODO it!</h1>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- scoped : 상위 부모 클래스 다 무시되고 컴포넌트 내에서만 css가 먹는다-->
+<style scoped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+
+</style>
---
+++ src/components/BaseInputbox.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function() {
+            console.log('addTodo ::', this.newTodoItem)
+            
+            localStorage.setItem(this.newTodoItem, this.newTodoItem)
+            // 저장하면 inputbox 초기화
+            this.newTodoItem = '';
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
