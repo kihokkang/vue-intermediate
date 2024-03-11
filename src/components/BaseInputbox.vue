efcb0f4188c30f067b9eec65af540b4217e2611e
GihoKim <kihokkang2@gmail.com>
2024-03-11 14:49:53 +0900

    Modal 팝업 구현

--- src/components/BaseInputbox.vue
+++ src/components/BaseInputbox.vue
@@ -4,21 +4,38 @@
         <span class="addContainer" v-on:click="addTodo">
             <i class="fa-solid fa-plus addBtn"></i>
         </span>
+
+        <Modal v-if="showModal" @close="showModal = false">
+            <h3 slot="header">
+                경고!
+                <i class="closeModalBtn fa-solid fa-xmark" @click="showModal = false"></i>
+            </h3>
+            <div slot="body">
+                무언가를 입력하세요
+            </div>
+        </Modal>
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
 export default {
     data: function() {
         return {
-            newTodoItem: ""
+            newTodoItem: "",
+            showModal: false
         }
+    },
+    components: {
+        Modal : Modal 
     },
     methods: {
         addTodo: function() {
             if (this.newTodoItem !== ''){
                 this.$emit('addTodoItem', this.newTodoItem);
                 this.clearInput();
+            } else {
+                this.showModal = !this.showModal;
             }  
         },
         clearInput: function() {
@@ -54,6 +71,9 @@ input:focus {
 .addBtn{
     color: white;
     vertical-align: middle;
+}
+.closeModalBtn {
+    color: #42b983
 }
 
 </style>
