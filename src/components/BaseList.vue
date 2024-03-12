9ef58d60598bff52b29d9b4726f3540f078b4565
GihoKim <kihokkang2@gmail.com>
2024-03-12 10:43:20 +0900

    리스트 effect 효과를 위한 transition 추가

--- src/components/BaseList.vue
+++ src/components/BaseList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="(todoItems, index) in propsdata" v-bind:key="todoItems.item" class="shadow">
         <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItems.completed}" 
           v-on:click="toggleComplete(todoItems, index)">
@@ -10,7 +10,7 @@
           <i class="fa-solid fa-trash"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -60,5 +60,14 @@ li {
 .textCompleted {
   text-decoration: line-through;
   color:#b3adad
+}
+
+/* 리스트 아이템 트랜지션 효과 */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
